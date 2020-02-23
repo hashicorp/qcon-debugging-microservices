b584617c59afa5350b0e4613d901afba052d8f23
Nic Jackson <jackson.nic@gmail.com>
2020-02-23 23:54:19 +0000

    Clean up apt repo

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,9 +2,14 @@
 set -e
 
 docker system prune -f
+yes | apt-get clean
+yes | apt-get autoremove --purge
+rm -rf /var/lib/apt/lists/*
 
 curl https://shipyard.run/install | bash 
 
 shipyard run --no-browser github.com/hashicorp/qcon-debugging-microservices//stack
 
 shipyard pause
+
+df -h
